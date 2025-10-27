4aeaff83eb9c76dab71bed7accb22b2cfcdfd0c7
Ethan Swaidner <83797581+eswaidner@users.noreply.github.com>
2025-10-27 13:17:43 -0600

    UI containers (#19)
    * UI Containers
    
    * Separate CSS and Tracking
    
    * Cleanup Container code
    
    ---------
    
    Co-authored-by: eleusink <157413961+eleusink@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 // import { useState, useRef } from "react";
 // import { useGlobalStore } from "./state";
-import css from "./App.module.css";
+import './index.css';
+import css from "./styles/App.module.css";
+import DegreePlanner from './containers/DegreePlanner';
+import ClassSection from './containers/ClassSection';
 
 // This is the root component of our app
 export default function App() {
@@ -9,7 +12,7 @@ export default function App() {
   // const exampleAction = useGlobalStore((state) => state.exampleAction);
   // exampleAction();
 
-  const title = "Degree Planner";
+  //const title = "Degree Planner";
 
   // The useState hook is how we can use reactive local state
   // const [localState, setLocalState] = useState<string>("");
@@ -25,7 +28,8 @@ export default function App() {
   return (
     // This is how you apply a style from a css module to an element
     <div className={css.app}>
-      <div>Title: {title}</div>
+      <DegreePlanner />
+      <ClassSection />
     </div>
   );
 }
---
+++ src/containers/ClassFilters.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from "./../styles/ClassSection.module.css";
+
+const ClassFilters: React.FC = () => {
+    return (
+        <div className = {css.classFilter}>
+            <h2>Filters</h2>
+        </div>
+    );
+}
+
+export default ClassFilters;
