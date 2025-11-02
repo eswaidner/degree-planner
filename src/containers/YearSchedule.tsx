229a47c12ea7de7190cbc61d7dc95d7a67db1481
Emily Johnson <emmyj122005@outlook.com>
2025-11-01 18:30:28 -0600

    years and semester boxes

--- src/containers/YearSchedule.tsx
+++ src/containers/YearSchedule.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
-import css from "./../styles/ClassSection.module.css";
+import css from "./../styles/DegreePlanner.module.css";
+
+function AddClassBtn () { 
+	function handleClick() { 
+		alert('Add Class Button Clicked') 
+	} 
+	return ( 
+		<button onClick={handleClick}> 
+			+ Add Class
+		</button> 
+	)
+}
+
+function RemoveYearBtn () { 
+	function handleClick() { 
+		alert('Remove Year Button CLicked')
+	} 
+	return (
+		<button onClick={handleClick}> 
+			-
+		</button> 
+	) 
+}
+
+function Semester () { 
+	return ( 
+		<div className = {css.semester}>
+			<h2><center>Fall</center></h2> 
+			<AddClassBtn /> <AddClassBtn /> <AddClassBtn /> 
+			<AddClassBtn /> <AddClassBtn /> <AddClassBtn /> 
+		</div> 
+	); 
+} 
+
+function YearHeader () { 
+	return ( 
+		<div className = {css.header}> 
+			<h2><center>2024</center></h2> 
+			<RemoveYearBtn />
+		</div> 
+	);
+}
+
+function Year () { 
+	return ( 
+		<div className = {css.year}> 
+			<YearHeader /> 
+			<Semester /> <Semester /> 
+		</div>
+	); 
+}
 
 const YearSchedule: React.FC = () => {
     return (
         <div className = {css.yearlySchedule}>
-            <h2>Yearly Schedule</h2>
+            <Year /> <Year /> <Year /> <Year />
         </div>
     );
 }
