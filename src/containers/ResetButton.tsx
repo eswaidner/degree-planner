f3f1c91f23b04674d01dccba7d1e34bb37e2b3b3
eswaidner <eswaidner2@gmail.com>
2025-11-27 12:05:33 -0700

    prod deploy setup

--- src/containers/ResetButton.tsx
+++ src/containers/ResetButton.tsx
@@ -1,32 +1,42 @@
-import React, { useState } from 'react';
 import bb_css from "./../styles/DegreePlanner.module.css";
 import m_css from "./../styles/Modal.module.css";
-import { useGlobalStore } from '../state';
+import { useGlobalStore } from "../state";
 
 export default function ResetButton() {
-    const setModalContent = useGlobalStore((s) => s.setModalContent);
-    const resetDegreePlan = useGlobalStore((s) => s.resetDegreePlan);
-    
-    const handleClick = () => {
-        setModalContent("resetModalKey",(
-            <div>
-                <h2>Confirm Reset</h2>
-                <div className={`${m_css.modalRow}`}>
-                    <button className={`btn ${m_css.modalBtn}`} onClick={() => {
-                        setModalContent("resetModalKey", null);
-                        resetDegreePlan();
-                    }}>Reset</button>
-                    <button className={`btn ${m_css.modalBtn}`} onClick={() => {
-                        setModalContent("resetModalKey", null);
-                    }}>Cancel</button>
-                </div>
-            </div>
-        ));
-    };
-  
+  const setModalContent = useGlobalStore((s) => s.setModalContent);
+  const resetDegreePlan = useGlobalStore((s) => s.resetDegreePlan);
+
+  const handleClick = () => {
+    setModalContent(
+      "resetModalKey",
+      <div>
+        <h2>Confirm Reset</h2>
+        <div className={`${m_css.modalRow}`}>
+          <button
+            className={`btn ${m_css.modalBtn}`}
+            onClick={() => {
+              setModalContent("resetModalKey", null);
+              resetDegreePlan();
+            }}
+          >
+            Reset
+          </button>
+          <button
+            className={`btn ${m_css.modalBtn}`}
+            onClick={() => {
+              setModalContent("resetModalKey", null);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>,
+    );
+  };
+
   return (
     <button onClick={handleClick} className={`btn ${bb_css.btnBarButtons}`}>
-        Reset
+      Reset
     </button>
   );
 }
--- src/containers/UploadFileBtn.tsx
+++ src/containers/UploadFileBtn.tsx
@@ -1,56 +1,61 @@
-import React from 'react';
 import css from "./../styles/DegreePlanner.module.css";
 import m_css from "./../styles/Modal.module.css";
-import { useGlobalStore } from '../state';
+import { useGlobalStore } from "../state";
 
 // Check if selected element is of type File
 interface Props {
-    onFileSelected: (file: File) => void;
+  onFileSelected: (file: File) => void;
 }
-
 
 // If a file is selected (event.target.files?.[0]), verify through Props
 // Have to clear <input> style (display: none) and override with <label>
 export default function UploadDegreeBtn({ onFileSelected }: Props) {
-    const setModalContent = useGlobalStore((s) => s.setModalContent);
+  const setModalContent = useGlobalStore((s) => s.setModalContent);
 
-    return (
-        <div>
-            <input
-                type="file"
-                accept='.html'
-                id="upload-degree-input" /* reference for reading file in fileInput */
-                style={{ display: 'none' }}
-                onChange={event => {
-                    /*If 1 file of type file is selected, set file var to selected file */
-                    const file = event.target.files?.[0];
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".html"
+        id="upload-degree-input" /* reference for reading file in fileInput */
+        style={{ display: "none" }}
+        onChange={(event) => {
+          /*If 1 file of type file is selected, set file var to selected file */
+          const file = event.target.files?.[0];
 
-                    if (file) {
-                        if (file.size === 0) {
-                            setModalContent("emptyFileWarning", (
-                                <div>
-                                    <h2>Warning: Empty File</h2>
-                                    <div className={m_css.modalRow}>
-                                        <button className={`btn ${m_css.modalBtn}`}
-                                            onClick={() => {
-                                                setModalContent("emptyFileWarning", null);
-                                            }}
-                                        >Close</button>
-                                    </div>
-                                </div>
-                            ));
+          if (file) {
+            if (file.size === 0) {
+              setModalContent(
+                "emptyFileWarning",
+                <div>
+                  <h2>Warning: Empty File</h2>
+                  <div className={m_css.modalRow}>
+                    <button
+                      className={`btn ${m_css.modalBtn}`}
+                      onClick={() => {
+                        setModalContent("emptyFileWarning", null);
+                      }}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>,
+              );
 
-                            console.log("File is empty");
-                            return;
-                        }
+              console.log("File is empty");
+              return;
+            }
 
-                        onFileSelected(file);
-                    } // if file END
-                }}
-            />
-            <label htmlFor="upload-degree-input" className={`btn ${css.btnBarButtons}`}>
-                + Upload Degree Audit
-            </label>
-        </div>
-    );
+            onFileSelected(file);
+          } // if file END
+        }}
+      />
+      <label
+        htmlFor="upload-degree-input"
+        className={`btn ${css.btnBarButtons}`}
+      >
+        + Upload Degree Audit
+      </label>
+    </div>
+  );
 }
