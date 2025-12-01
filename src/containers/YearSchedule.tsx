884c2cd2bd7f3fe8f966d17fcfe61ccbfcc922bc
eswaidner <eswaidner2@gmail.com>
2025-11-30 18:17:03 -0700

    fixed degree audit years being removable

--- src/containers/YearSchedule.tsx
+++ src/containers/YearSchedule.tsx
@@ -5,7 +5,71 @@ import {
 } from "../state";
 import css from "./../styles/DegreePlanner.module.css";
 
-function AddClassBtn({ slotIndex }: { slotIndex: number }) {
+export default function YearlySchedule() {
+  const startYear = useGlobalStore((s) => s.startYear);
+  const numYears = useGlobalStore((s) => s.numYears);
+  const years = Array.from({ length: numYears }, (_, i) => startYear + i);
+
+  return (
+    <div className={css.yearlySchedule}>
+      {years.map((year, i) => (
+        <Year key={i} year={year} startingSlot={i * CLASS_SLOTS_PER_YEAR} />
+      ))}
+    </div>
+  );
+}
+
+function Year({ year, startingSlot }: { year: number; startingSlot: number }) {
+  const nextYear = year + 1;
+  return (
+    <div className={css.year}>
+      <div className={css.yearText}>
+        <h2>
+          <center>
+            {year}-{nextYear}
+          </center>
+        </h2>
+      </div>
+      <Semester semester={"Fall"} startingSlot={startingSlot} year={year} />
+      <Semester
+        semester={"Spring"}
+        startingSlot={startingSlot + CLASS_SLOTS_PER_SEMESTER}
+        year={nextYear}
+      />
+    </div>
+  );
+}
+
+function Semester({
+  semester,
+  startingSlot,
+  year,
+}: {
+  semester: string;
+  startingSlot: number;
+  year: number;
+}) {
+  const classSlots = Array.from(
+    { length: CLASS_SLOTS_PER_SEMESTER },
+    (_, i) => i,
+  );
+
+  return (
+    <div className={css.semester}>
+      <h2>
+        <center>
+          {semester} {year}
+        </center>
+      </h2>
+      {classSlots.map((classSlot, i) => {
+        const slotIndex = startingSlot + classSlot;
+        return <ClassSlot key={i} slotIndex={slotIndex} />;
+      })}
+    </div>
+  );
+}
+
+function ClassSlot({ slotIndex }: { slotIndex: number }) {
   const classToAdd = useGlobalStore((s) => s.classToAdd);
   const setClassToAdd = useGlobalStore((s) => s.setClassToAdd);
   const classSlot = useGlobalStore((s) => s.classSlots[slotIndex]);
@@ -45,69 +109,3 @@ function AddClassBtn({ slotIndex }: { slotIndex: number }) {
     </button>
   );
 }
-
-function Semester({
-  semester,
-  startingSlot,
-  year,
-}: {
-  semester: string;
-  startingSlot: number;
-  year: number;
-}) {
-  const classSlots = Array.from(
-    { length: CLASS_SLOTS_PER_SEMESTER },
-    (_, i) => i,
-  );
-
-  return (
-    <div className={css.semester}>
-      <h2>
-        <center>
-          {semester} {year}
-        </center>
-      </h2>
-      {classSlots.map((classSlot, i) => {
-        const slotIndex = startingSlot + classSlot;
-        return <AddClassBtn key={i} slotIndex={slotIndex} />;
-      })}
-    </div>
-  );
-}
-
-function Year({ year, startingSlot }: { year: number; startingSlot: number }) {
-  const nextYear = year + 1;
-  return (
-    <div className={css.year}>
-      <div className={css.yearText}>
-        <h2>
-          <center>
-            {year}-{nextYear}
-          </center>
-        </h2>
-      </div>
-      <Semester semester={"Fall"} startingSlot={startingSlot} year={year} />
-      <Semester
-        semester={"Spring"}
-        startingSlot={startingSlot + CLASS_SLOTS_PER_SEMESTER}
-        year={nextYear}
-      />
-    </div>
-  );
-}
-
-const YearlySchedule: React.FC = () => {
-  const startYear = useGlobalStore((s) => s.startYear);
-  const numYears = useGlobalStore((s) => s.numYears);
-  const years = Array.from({ length: numYears }, (_, i) => startYear + i);
-
-  return (
-    <div className={css.yearlySchedule}>
-      {years.map((year, i) => (
-        <Year key={i} year={year} startingSlot={i * CLASS_SLOTS_PER_YEAR} />
-      ))}
-    </div>
-  );
-};
-
-export default YearlySchedule;
